76f30fe93e24e0f6b84c4d45448180dafa59a17e
KislyjKisel <danilakisel@gmail.com>
2023-11-15 20:14:52 +0300

    cc config

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ import Lake
 open System Lake DSL
 
 def leanSoureDir := "lib"
-def cCompiler := "cc"
+def cCompiler := get_config? cc
 def cDir : FilePath := "c"
 def ffiSrc := cDir / "ffi.c"
 def ffiO := "ffi.o"
@@ -28,6 +28,10 @@ extern_lib «mysql-ffi» pkg := do
   let libFile := pkg.dir / defaultBuildDir / cDir / ffiLib
   let oFile := pkg.dir / defaultBuildDir / cDir / ffiO
   let srcTarget ← inputFile <| pkg.dir / ffiSrc
-  let weakArgs := #["-I", (← getLeanIncludeDir).toString]
+  let mut weakArgs := #["-I", (← getLeanIncludeDir).toString]
   let traceArgs := #["-I", includesDir]
-  buildStaticLib libFile #[← buildO "ffi.c" oFile srcTarget weakArgs traceArgs cCompiler]
+  let mut cc ← getLeanCc
+  match cCompiler with
+  | some cCompiler => cc := cCompiler
+  | none => weakArgs := weakArgs ++ #["-I", (← getLeanIncludeDir) / "clang" |>.toString]
+  buildStaticLib libFile #[← buildO "ffi.c" oFile srcTarget weakArgs traceArgs cc]
